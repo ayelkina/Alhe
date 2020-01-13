6da1f6f8328a4ed154c8d38006badb0fc6187d32
ayelkina <yelkina.anna@gmail.com>
2020-01-13 22:26:55 +0100

    Optimized a*, added input data set

--- Astar.py
+++ Astar.py
@@ -1,4 +1,7 @@
 import copy
+
+from networkx import Graph
+
 from Path import Path
 
 
@@ -6,16 +9,14 @@ class Astar:
     expanded_nodes = 0
     solution = []
 
-    def solve(self, graph, dist):
-        input = list(graph.nodes)
-        nodes_number = len(input)
-        first_node = input[0]
-        edges = list(dist)
-        edges.sort()
+    def solve(self, graph: Graph, edges: list):
+        input_nodes = list(graph.nodes)
+        nodes_number = len(input_nodes)
+        first_node = input_nodes[0]
         open_set = [Path(0, 0, [first_node], edges)]
 
         while open_set:
-            self.solution = self.minimal_solution(open_set)
+            self.solution: Path = self.minimal_solution(open_set)
             last_node = self.solution.path[-1]
             if len(self.solution.path) == nodes_number and last_node == first_node:
                 break
@@ -27,15 +28,15 @@ class Astar:
                 if node in self.solution.path: continue
                 sum_cost = self.solution.cost + cost
                 new_path = self.solution.path + [node]
-                not_used_edges = copy.deepcopy(self.solution.not_used_edges)
+                not_used_edges = self.solution.not_used_edges[:]
                 not_used_edges.remove(cost)
                 heuristic = self.heuristic_fun(not_used_edges, nodes_number, len(new_path)-1)
                 open_set.append(Path(sum_cost, heuristic, new_path, not_used_edges))
             self.expanded_nodes += 1
 
     @staticmethod
-    def minimal_solution(open_set):
-        min_cost = 9999999
+    def minimal_solution(open_set: list):
+        min_cost = 9999999999
         solution = ''
         for state in open_set:
             if state.goal < min_cost:
@@ -45,10 +46,10 @@ class Astar:
         return solution
 
     @staticmethod
-    def heuristic_fun(edges, n, k):
+    def heuristic_fun(edges: list, n: int, k: int):
         sum = 0
-        minimal_edges = edges[:(n-k)]
-        for edge in minimal_edges:
-            sum += edge
+        nodes_left = n-k
+        for index in range(nodes_left):
+            sum += edges[index]
 
         return sum
--- Bruteforce.py
+++ Bruteforce.py
@@ -3,19 +3,19 @@ from networkx import Graph
 
 class Bruteforce:
 
-    min_cost = 9999999
+    min_cost = 9999999999
     solution = []
+    count = 0
 
     def solve(self, graph: Graph, first_node):
-        i = 0
-        self.min_cost = 9999999
+        self.min_cost = 9999999999
         self.solution = []
         self._solve(graph, first_node, first_node, [], 0)
-        i += 1
         print('cost={}, solution={}'.format(self.min_cost, self.solution))
+        print(self.count)
 
     def _solve(self, graph: Graph, first_node, cur_node, visited_nodes: list, cost):
-
+        self.count += 1
         if len(visited_nodes) == len(graph.nodes):
             if visited_nodes[-1] == first_node and cost < self.min_cost:
                 self.min_cost = cost
