112d2bc4678d801a94709f1951d8837bd47c52d9
semidude <radoslaw.panuszewski15@gmail.com>
2020-01-14 00:23:18 +0100

    Delete unnecessary files

--- Node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    id = ''
-    long = ''
-    lat = ''
-
-    def __init__(self, id, long, lat):
-        self.id = int(id)
-        self.long = float(long)
-        self.lat = float(lat)
